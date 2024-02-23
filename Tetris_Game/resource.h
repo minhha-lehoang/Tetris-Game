36b32f07559f9f2ac48890ce7c221db511c32c9e
Lê Hoàng Minh Hà <minhha.lehoang@gmail.com>
2024-02-23 14:55:16 +0700

    Add files via upload

---
+++ Tetris_Game/resource.h
@@ -0,0 +1,48 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Tetris_Game.rc
+//
+#define IDB_PNG1                        101
+#define IDB_PNG2                        102
+#define IDB_PNG3                        103
+#define IDB_PNG4                        105
+#define IDB_PNG5                        106
+#define IDB_PNG6                        107
+#define IDB_PNG7                        108
+#define IDB_PNG8                        109
+#define IDB_PNG9                        110
+#define IDB_PNG10                       111
+#define IDB_PNG11                       112
+#define IDB_PNG12                       113
+#define IDB_PNG13                       114
+#define IDB_PNG14                       115
+#define IDB_PNG15                       116
+#define IDB_PNG16                       117
+#define IDB_PNG17                       118
+#define IDB_PNG18                       119
+#define IDB_PNG19                       120
+#define IDB_PNG20                       121
+#define IDB_PNG21                       122
+#define IDB_PNG22                       123
+#define IDR_WAVE1                       124
+#define IDR_WAVE2                       125
+#define IDR_WAVE3                       126
+#define IDR_WAVE4                       127
+#define IDR_WAVE5                       128
+#define IDR_WAVE6                       129
+#define IDR_MP31                        130
+#define IDR_TXT1                        131
+#define IDR_RIBBON1                     132
+#define IDR_TTF1                        133
+#define ID_BUTTON2                      40001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
